3044cb20bd1acc7c1e7c6105a6a5f4ec8f3dec54
pikumin6 <pikumin@hotmail.co.jp>
2026-06-10 01:00:09 +0900

    バッチファイルによる同人レビュー自動更新

--- app.js
+++ app.js
@@ -155,7 +155,8 @@ async function main() {
                     // 💡【追加】テンプレート側で試し読みリンクが壊れないよう、生のURLをそのまま引き渡す
                     //sampleUrl: product.sampleUrl || '',
                     //sampleLink: product.sampleUrl || '',
-                    	
+                   	sampleUrl: product.sampleURL || '',
+                    		
                     description: product.description || '羞恥系おすすめの最新コミックです。',
                     reviewRating: detailData.reviewRating || product.review?.rating || '0.0',
                     reviewCount: detailData.reviewCount || product.review?.count || 0,
