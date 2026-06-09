42097906b55e772ea4d4af41210e54109361d292
pikumin6 <pikumin@hotmail.co.jp>
2026-06-10 01:14:30 +0900

    バッチファイルによる同人レビュー自動更新

--- src/template.js
+++ src/template.js
@@ -252,14 +252,6 @@ function generateSinglePostHTML(article, siteTitle, recommendArticles = []) {
             </div>
         </article>
     </main>
-    			
-	<footer class="bg-slate-900 text-slate-400 py-8 text-center text-xs mt-12 w-full">
-        <div class="max-w-4xl mx-auto px-4">
-            <p class="text-slate-500 mb-2">Supported by DMMアフィリエイト</p>
-            <p>&copy; 2026 ${siteTitle}. All Rights Reserved.</p>
-        </div>
-    </footer>
-    			
     ${bypassScript}
 </body>
 </html>`;
@@ -491,6 +483,14 @@ function generateTopPageHTML(articles, displayDate, allTags, siteTitle) {
         }
     });
     </script>
+    	
+	<footer class="bg-slate-900 text-slate-400 py-8 text-center text-xs mt-12 w-full">
+        <div class="max-w-4xl mx-auto px-4">
+            <p class="text-slate-500 mb-2">Supported by DMMアフィリエイト</p>
+            <p>&copy; 2026 ${siteTitle}. All Rights Reserved.</p>
+        </div>
+    </footer>
+    	
     ${bypassScript}
 </body>
 </html>`;
