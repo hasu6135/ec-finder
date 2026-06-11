0d1b17ccb63b0c031371b1ddd318da88c8ac16b3
pikumin6 <pikumin@hotmail.co.jp>
2026-06-11 23:35:06 +0900

    バッチファイルによる同人レビュー自動更新

--- src/dmmApi.js
+++ src/dmmApi.js
@@ -23,7 +23,8 @@ async function fetchDmmProducts(apiId, affiliateId, siteTitle, fetchCount) {
         url.searchParams.append('floor', 'comic');
         url.searchParams.append('keyword', searchKeyword);
         url.searchParams.append('hits', hitsCount.toString());
-        url.searchParams.append('sort', 'rank');
+        url.searchParams.append('sort', 'rank'); //rank:売上順、review:評価順、date:新着順
+        url.searchParams.append('offset', 100);　   //ずらして検索
         url.searchParams.append('output', 'json');
 
         const response = await fetch(url.toString());
