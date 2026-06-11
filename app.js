2f62750f8713e0fc47e6c4a9a57095f3bd9d32bd
pikumin6 <pikumin@hotmail.co.jp>
2026-06-11 23:30:11 +0900

    バッチファイルによる同人レビュー自動更新

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { generateSinglePostHTML, generateTagPageHTML, generateTopPageHTML } = req
  * ===================================================
  */
 const SITE_TITLE = '羞恥系コミック';
-const FETCH_COUNT = 200;       // 💡 ここで指定した件数分、新着を一気にループ処理します！
+const FETCH_COUNT = 1;       // 💡 ここで指定した件数分、新着を一気にループ処理します！
 const ARCHIVE_DIR = 'archive';
 const TAGS_DIR = 'tags';
 const DB_FILE = 'db.json';   // 過去データを保存する簡易データベースファイル
